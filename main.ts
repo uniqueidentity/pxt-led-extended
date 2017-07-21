596ac9ebc93c9063fe5f447406840966301d65a2
uniqueidentity <30289647+uniqueidentity@users.noreply.github.com>
2017-07-21 10:24:01 +0100

    New functionality

--- main.ts
+++ main.ts
@@ -8,18 +8,19 @@ namespace ledext {
     let suspended: boolean;
 
     /**
-     * Sets a pixel to the specified brightness
+     * Turns on a pixel to the specified brightness
      * @param x horizontal coordinate, eg: 2
      * @param y vertical coordinate, eg: 2
+     * @param l brightness level, eg: 255
      */
     //% weight=60 blockGap=8
-    //% blockId=ledext_set block="set pixel at|x: %x|y: %y|to brightness:%b"
+    //% blockId=ledext_set block="set pixel at|x: %x|y: %y|to level:%l"
     //% parts="ledmatrix"
-    export function set(x: number, y: number, b: number) {
+    export function set(x: number, y: number, l: number): void {
         x = Math.clamp(0, 4, x);
         y = Math.clamp(0, 4, y);
-        b = Math.clamp(0, 255, b);
-        img.setPixelBrightness(x, y, b);
+        b = Math.clamp(0, 255, l);
+        img.setPixelBrightness(x, y, l);
         refresh();
     }
 
@@ -31,17 +32,41 @@ namespace ledext {
     //% weight=60 blockGap=8
     //% blockId=ledext_unset block="unset pixel at|x: %x|y: %y"
     //% parts="ledmatrix"
-    export function unset(x: number, y: number) {
+    export function unset(x: number, y: number): void {
         set(x, y, 0);
     }
 
+    /**
+     * Gets the brightness of a pixel
+     * @param x horizontal coordinate, eg: 2
+     * @param y vertical coordinate, eg: 2
+     */
+    //% weight=55 blockGap=8
+    //% blockId=ledext_get block="get level of pixel at|x: %x|y: %y"
+    //% parts="ledmatrix"
+    export function get(x: number, y: number): number {
+        return img.pixelBrightness(x,y)
+    }
+    
+    /**
+     * Gets the state of a pixel
+     * @param x horizontal coordinate, eg: 2
+     * @param y vertical coordinate, eg: 2
+     */
+    //% weight=55 blockGap=8
+    //% blockId=ledext_isOn block="get state of pixel at|x: %x|y: %y"
+    //% parts="ledmatrix"
+    export function isOn(x: number, y: number): boolean {
+        return img.pixelBrightness(x,y)>0;
+    }
+    
     /**
      * Clears the display
      */
     //% weight=50 blockGap=8
     //% blockId=ledext_clear block="clear"
     //% parts="ledmatrix"
-    export function clear() {
+    export function clear(): void {
         let r: number, c: number;
         suspend();
         for (r = 0; r < 5; r++) {
@@ -58,8 +83,8 @@ namespace ledext {
     //% weight=50 blockGap=8
     //% blockId=ledext_suspend block="suspend"
     //% parts="ledmatrix"
-    export function suspend() {
-        suspend = true;
+    export function suspend(): void {
+        suspended = true;
     }
         
     /**
@@ -68,8 +93,8 @@ namespace ledext {
     //% weight=40 blockGap=8
     //% blockId=ledext_resume block="resume"
     //% parts="ledmatrix"
-    export function resume() {
-        suspend = false;
+    export function resume(): void {
+        suspended = false;
         refresh();
     }
     
@@ -79,7 +104,7 @@ namespace ledext {
     //% weight=50 blockGap=8
     //% blockId=ledext_refresh block="refresh"
     //% parts="ledmatrix"
-    export function refresh():void {
+    export function refresh(): void {
         if (suspended) return;
         img.plotImage(0);  
     }
