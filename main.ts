206617e5edb425d75faffde3138124d143ceb921
uniqueidentity <30289647+uniqueidentity@users.noreply.github.com>
2017-07-22 08:59:25 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -137,16 +137,15 @@ namespace led_extended {
      * Animates an image presented as an array of strings.  Each string represents a row and each character the state of a pixel. 
      * Valid characters include 0-9 to represent the brightness of a pixel, F/f to represent a flashing pixel or anything else to
      * represent the pixel being off.
-     * @param image, eg: 
-     * @param scrollDirection, eg: scrollDirectionStyle.RightToLeft
-     * @param inputDirection, eg: inputDirectionStyle.FirstToLast
-     * @param animation, eg: animationStyle.None
-     * @param gap, eg: 200
+     * @param scrollDirection eg: scrollDirectionStyle.RightToLeft
+     * @param inputDirection eg: inputDirectionStyle.FirstToLast
+     * @param animation eg: animationStyle.None
+     * @param gap eg: 200
      */
     //% weight=55 blockGap=8
     //% blockId=ledext_animateFrames block="animate |image: %image| direction: %scrollDirection| reading: %inputDirection| style: %animation| ms between frames: %gap"
     //% parts="ledmatrix"
-    export function animateFrames(image: string[], scrollDirection: scrollDirectionStyle, inputDirection: inputDirectionStyle, animation: animationStyle, gap: number = 200) {
+    export function animateFrames(image: string[], scrollDirection: scrollDirectionStyle, inputDirection: inputDirectionStyle, animation: animationStyle, gap: number) {
         if (image == null) return;
         if (image.length == 0) return;
         let frames: number;
